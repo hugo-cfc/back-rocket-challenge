aae239090d37f4bbe80668a403897fac488763e4
Hugo Cesar Firmino Costa <hugo.costa@tce.al.gov.br>
2021-06-28 14:06:17 -0300

    Desafio completo

---
+++ src/server.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const { uuid } = require("uuidv4");
+
+const app = express();
+app.use(express.json());
+
+const repositories = [];
+
+app.get("/repositories", (req, res) => {
+  res.json(repositories);
+});
+
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  res.send(repository);
+});
+
+app.put("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+
+  const { title, url, techs, like } = req.body;
+
+  const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return res.status(400).json({ error: "repository not found" });
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  res.send(repository);
+});
+
+app.post("/repositories/:id/like", (req, res) => {
+  const { id } = req.params;
+
+  const repository = repositories.find((repository) => repository.id === id);
+
+  if (!repository) return res.status(400).json({ error: "repository not found" });
+
+  repository.likes += 1;
+
+  res.send(repository);
+});
+
+app.delete("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+
+  const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return res.status(400).json({ error: "repository not found" });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return res.status(200).send();
+});
+
+app.listen(3333, () => {
+  console.log("🚀 Back-end started");
+});
